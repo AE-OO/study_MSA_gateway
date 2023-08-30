6a849aa3d00f0bb64670499f2f7ea074207c71fe
ParkChaeWon <aeoo@naonsoft.com>
2023-08-30 17:25:42 +0900

    Section7. Configuration Service - Users Microservice에서 Spring Cloud Config 연동
    - 20230830

--- src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,13 +1,24 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayServiceApplication.class, args);
+	}
+
+	/**
+	 * actuator의 end point로 httptrace를 사용하기 위해 bean으로 등록
+	 */
+	@Bean
+	public HttpTraceRepository httpTraceRepository(){
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
